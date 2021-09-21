4a9f9b9b2aeeb7051ef6a7be9f6c63810fef946b
varas-eternity <varas8eternity@gmail.com>
2021-09-21 14:26:48 +0300

    grid-project_sixth-version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 
-  $('.header-navbar__btn').on('click', function () {
-    $(this).toggleClass('header-navbar__btn--active');
-    $('.header-navbar__list').toggleClass('header-navbar__list--active');
-  });
+    $('.header-navbar__btn').on('click', function () {
+        $(this).toggleClass('header-navbar__btn--active');
+        $('.header-navbar__list').toggleClass('header-navbar__list--active');
+    });
 
 });
